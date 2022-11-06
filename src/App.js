eefcb98ff360b8d67fe3f3bf5a6d09218543c71c
Tom <tomcat-26@hotmail.co.uk>
2022-11-06 11:13:59 +0800

    Initial testing suite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [heading, setHeading] = useState('Magnificent Monkeys');
+
+    const clickHandler = () => {
+        setHeading('Radical Rhinos');
+    }
+
+    return (
+        <>
+            <button type="button" onClick={clickHandler}>
+                Click me
+            </button>
+            <h1>{heading}</h1>;
+        </>
+    );  
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// describe("App component", () => {
+//   it("renders correct heading", () => {
+//     const { getByRole } = render(<App />);
+//     expect(getByRole("heading").textContent).toMatch(/our first test/i);
+//   });
+// });
+
+describe("App component", () => {
+  it('renders Magnificent Monkeys', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button click", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: 'Click me' });
+
+    userEvent.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+  });
 });
